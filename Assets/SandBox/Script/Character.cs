27872ca670404c546f8860a9703f3f879e17358f
LucaGalofaro <lucagalofaro@outlook.it>
2024-11-25 17:15:50 +0100

    TMP e cartelle

--- Assets/SandBox/Script/Character.cs
+++ Assets/SandBox/Script/Character.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     CharacterController _characterController;
+    Interactable _interactable;
+
     [SerializeField] float moveSpeed;
     [SerializeField] float rotSpeed;
+
+
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
@@ -16,12 +20,77 @@ public class Character : MonoBehaviour
 
     private void Update()
     {
+        UpdatePosition();
+        UpdateRotation();
+        CheckInputs();
+    }
+
+    void UpdatePosition()
+    {
         var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         dir.Normalize();
         dir *= moveSpeed * Time.deltaTime;
         dir = transform.TransformVector(dir);
         _characterController.SimpleMove(dir);
+    }
+
+    void UpdateRotation()
+    {
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotSpeed);
         Camera.main.transform.Rotate(Vector3.right * -Input.GetAxis("Mouse Y") * rotSpeed);
+    }
+
+    void CheckInteractables()
+    {
+        Physics.SphereCast(Camera.main.transform.position, .25f, Camera.main.transform.forward, out RaycastHit hit, 2, LayerMask.GetMask("Interactable"));
+        if (hit.collider != null)
+        {
+            var newInteractable = hit.collider.gameObject.GetComponentInParent<Interactable>();
+
+            if (newInteractable == _interactable)
+                return;
+
+            if (_interactable != null)
+                _interactable.HideTooltip();
+
+            _interactable = newInteractable;
+            _interactable.ShowTooltip();
+        }
+        else if (_interactable != null)
+        {
+            _interactable.HideTooltip();
+            _interactable = null;
+        }
+    }
+
+    void CheckTalkables()
+    {
+
+    }
+
+    void CheckInputs()
+    {
+        CheckTalk();
+        CheckInteraction();
+    }
+
+    void CheckTalk()
+    {
+        CheckTalkables();
+        //Press F to talk
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+    }
+
+    void CheckInteraction()
+    {
+        CheckInteractables();
+        //Press E to Interact
+        if (Input.GetKeyDown(KeyCode.E) && _interactable != null)
+        {
+            _interactable.Interact();
+        }
     }
 }
---
+++ Assets/SandBox/Script/Interactable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] GameObject tooltip;
+
+    private void Awake()
+    {
+        tooltip.SetActive(false);
+    }
+
+    public void ShowTooltip()
+    {
+        tooltip.SetActive(true);
+    }
+
+    public void HideTooltip()
+    {
+        tooltip.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        Destroy(transform.root.gameObject);
+    }
+}
